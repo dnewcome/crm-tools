840faf1176bb2b6454f2b8365865cc78902e780a
dnewcome <djn125@yahoo.com>
2011-11-18 14:24:35 -0800

    refactored crm tools to a core library

---
+++ CrmToolsCore/MetaData/Serializer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Runtime.Serialization.Json;
+using System.Runtime.Serialization;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CrmMetadataGenerator
+{
+	/**
+	* TODO: use the generic CrmJsonSerializer instead of this specific serializer
+	*/
+	public class Serializer
+	{
+		public static EntityMetadataSpec DeSerialize( string json ) {
+			DataContractJsonSerializer ser = new DataContractJsonSerializer( typeof( EntityMetadataSpec ) );
+			EntityMetadataSpec ent = ( EntityMetadataSpec )ser.ReadObject(
+				new MemoryStream( System.Text.Encoding.UTF8.GetBytes( json ) )
+			);
+			return ent;
+		}
+
+		public static string Serialize( EntityMetadataSpec entity ) {
+			DataContractJsonSerializer ser = new DataContractJsonSerializer( typeof( EntityMetadataSpec ) );
+			MemoryStream ms = new MemoryStream();
+			ser.WriteObject( ms, entity );
+			ms.Seek( 0, SeekOrigin.Begin );
+			return new StreamReader( ms ).ReadToEnd();
+		}
+	}
+}
